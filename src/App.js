aa90e9d393b68596915bc2536ccc943939cb2574
nicatzamanwork <webdevelopernicat@gmail.com>
2022-12-25 23:00:04 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Users from "./Users";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="textarea"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <ReactMarkdown children={input} className="markdown" />
+      <Users value={input} />
     </div>
   );
 }
+
+// const Component = ({ value, language }) => {
+//   const codeString = "(num) => num + 1";
+//   return (
+//     <SyntaxHighlighter language={language} style={docco}>
+//       {value}
+//     </SyntaxHighlighter>
+//   );
+// };
 
 export default App;
---
+++ src/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Users(props) {
+  return <div>{props.value}</div>;
+}
+
+export default Users;
